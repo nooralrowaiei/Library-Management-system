d9dfef9561ed0fb408a39aa0df5dbe6165fe4aad
nooralrowaiei <noor.alrowaiei00@gmail.com>
2025-12-26 03:41:22 +0500

    issue fixing

--- include/dbcon.php
+++ include/dbcon.php
@@ -1,10 +1,12 @@
 <?php
-$host = "projectlibrary.database.windows.net";
+$host = "projectlibrary.mysql.database.azure.com";
 $user = "noor";
 $pass = "admin@434";
 $db   = "project_library";
 
 $mysqli = mysqli_init();
 mysqli_ssl_set($mysqli, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-mysqli_real_connect($mysqli, $host, $user, $pass, $db, 3306, NULL, MYSQLI_CLIENT_SSL);
-
+if (!mysqli_real_connect($mysqli, $host, $user, $pass, $db, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+?>
