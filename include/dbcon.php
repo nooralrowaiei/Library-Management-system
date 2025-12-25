6b6a25711271db6fc7d642812cf9194f85deacff
nooralrowaiei <noor.alrowaiei00@gmail.com>
2025-12-26 03:48:56 +0500

    issue fix

--- include/dbcon.php
+++ include/dbcon.php
@@ -4,9 +4,9 @@ $user = "noor";
 $pass = "admin@434";
 $db   = "project_library";
 
-$mysqli = mysqli_init();
-mysqli_ssl_set($mysqli, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-if (!mysqli_real_connect($mysqli, $host, $user, $pass, $db, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+$con = mysqli_init();
+mysqli_ssl_set($con, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+if (!mysqli_real_connect($con, $host, $user, $pass, $db, 3306, NULL, MYSQLI_CLIENT_SSL)) {
     die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
 }
 ?>
