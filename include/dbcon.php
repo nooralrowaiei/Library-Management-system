2d28fbe9f436d255e61daa744928a00a24f6fd22
nooralrowaiei <noor.alrowaiei00@gmail.com>
2025-12-26 03:33:46 +0500

    issue fix

--- include/dbcon.php
+++ include/dbcon.php
@@ -7,4 +7,4 @@ $db   = "project_library";
 $mysqli = mysqli_init();
 mysqli_ssl_set($mysqli, NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
 mysqli_real_connect($mysqli, $host, $user, $pass, $db, 3306, NULL, MYSQLI_CLIENT_SSL);
-?>
+
